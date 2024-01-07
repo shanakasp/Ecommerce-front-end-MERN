8156952b33cf23de57ffa5232542d453aa544de2
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-01-07 14:15:41 +0530

    modified Product.jsx in Components

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../Nav";
 
-function Product() {
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    try {
+      const result = await fetch("http://localhost:5000/products");
+      const data = await result.json();
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  console.warn(products);
+
   return (
     <div>
-      <NavBar></NavBar>Product
+      <NavBar />
+      <h2>Product List</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            {product.name} - {product.price} - {product.category} -{" "}
+            {product.company} - UserId: {product.userId}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default Product;
+export default ProductList;
